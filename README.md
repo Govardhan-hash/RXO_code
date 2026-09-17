4801beee9016cea3214faff12a12a61d8c8d8f2b
Govardhan-hash <vangavarugugovardhanreddy@gmail.com>
2026-09-17 20:30:06 +0530

    modified:   README.md modified:   main.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RXO_code
+# Welcome to RXO this is just OX in Rotation.
+# Rules and how to play: first each player takes turns placing their O/X like tic tac toe, then after placing we
+# an move the O/X. Winning can only be done after MOVING 2 times(after 5 turns), and winning pattern is same as
+# tic tac toe
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Welcome to RXO this is just OX in Rotation.
+"""Rules and how to play: first each player takes turns placing their O/X like tic tac toe, then after placing we
+can move the O/X. Winning can only be done after MOVING 2 times(after 5 turns), and winning pattern is same as
+tic tac toe"""
 
 
 Board = {'c1': " ", "a": "A", "b": "B", "c": "C",
